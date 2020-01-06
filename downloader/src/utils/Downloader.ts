c955b720b4dfdcea2c287b7f02649c1856080bd7
Gurdev S. Panue <gpanue@hotmail.com>
2020-01-06 23:22:48 +0000

    Implemented downloader

---
+++ downloader/src/utils/Downloader.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+/**
+ * @name Downloader
+ * @description Class for downloading data from an API.
+ */
+class Downloader {
+  api: string;
+  params: object;
+
+  constructor(api: string, params: object) {
+    this.api = api;
+    this.params = params;
+  }
+
+  async getData(): Promise<object> {
+    try {
+      return await axios.get(this.api, { ...this.params });
+    }
+    catch (error) {
+      console.log(`Could not get data from the following web service: ${this.api}`);
+      console.log(`Reason: ${error.message}`);
+
+      return null;
+    }
+  }
+}
+
+export default Downloader;
