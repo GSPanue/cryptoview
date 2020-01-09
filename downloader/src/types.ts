1772582c2c4d8fae220bac927dd1cb15872aa76d
Gurdev S. Panue <gpanue@hotmail.com>
2020-01-09 23:27:30 +0000

    Added types for CoinMarketCap and coinlayer

--- downloader/src/types.ts
+++ downloader/src/types.ts
@@ -2,3 +2,24 @@
 interface Response {
   [key: string]: any
 }
+
+// CoinMarketCap Object
+interface coinMarketCapObject {
+  [key: string]: {
+    quote: {
+      USD: {
+        price: number
+      }
+    }
+  }
+}
+
+// coinlayer Object
+interface coinlayerObject {
+  rates: {
+    BTC: number,
+    ETH: number,
+    LTC: number,
+    XRP: number
+  }
+}
