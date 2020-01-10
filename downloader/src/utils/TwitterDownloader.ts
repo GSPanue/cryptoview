f257824d6790ddb62cb98556c3fcb805cd2f0ccf
Gurdev S. Panue <gpanue@hotmail.com>
2020-01-10 21:09:33 +0000

    Created TwitterDownloader class

---
+++ downloader/src/utils/TwitterDownloader.ts
@@ -0,0 +1,44 @@
+import * as Twitter from 'twitter';
+
+/**
+ * @name TwitterDownloader
+ * @description Class for downloading data from Twitter.
+ */
+class TwitterDownloader {
+  client: Twitter;
+
+  constructor(
+    consumerKey: string,
+    consumerSecret: string,
+    accessTokenKey: string,
+    accessTokenSecret: string
+    ) {
+      this.client = new Twitter({
+        consumer_key: consumerKey,
+        consumer_secret: consumerSecret,
+        access_token_key: accessTokenKey,
+        access_token_secret: accessTokenSecret
+      });
+  }
+
+  async searchTweets(keyword: string): Promise<object> {
+    try {
+      const result = await this.client.get('search/tweets', {
+        q: keyword,
+        count: 10,
+        lang: 'en',
+        result_type: 'recent'
+      });
+
+      return result;
+    }
+    catch (error) {
+      console.log('Could not get tweets.');
+      console.log(`Reason: ${error[0].message}`);
+
+      return Promise.reject(new Error(error[0].message));
+    }
+  }
+}
+
+export default TwitterDownloader;
--- downloader/src/utils/index.ts
+++ downloader/src/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as DBInterface } from './DBInterface';
 export { default as APIDownloader } from './APIDownloader';
+export { default as TwitterDownloader } from './TwitterDownloader';
