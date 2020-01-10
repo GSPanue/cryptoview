39901d5d511b1e95a2a37ec5d624b2a05aa35b44
Gurdev S. Panue <gpanue@hotmail.com>
2020-01-10 02:43:45 +0000

    Removed redundant types

--- downloader/src/types.ts
+++ downloader/src/types.ts
@@ -3,23 +3,9 @@ interface Response {
   [key: string]: any
 }
 
-// CoinMarketCap Object
-interface coinMarketCapObject {
-  [key: string]: {
-    quote: {
-      USD: {
-        price: number
-      }
-    }
-  }
-}
-
-// coinlayer Object
-interface coinlayerObject {
-  rates: {
-    BTC: number,
-    ETH: number,
-    LTC: number,
-    XRP: number
-  }
+// CoinCap Object
+interface CoinCapObject {
+  id: string;
+  symbol: string;
+  priceUsd: string;
 }
