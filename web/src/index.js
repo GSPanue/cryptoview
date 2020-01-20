620fb307f8996a46415d3a7770727fdaa4a657d0
Gurdev S. Panue <gpanue@hotmail.com>
2020-01-20 18:13:03 +0000

    Imported store

--- web/src/index.js
+++ web/src/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import App from '@/App';
+import store from '@/store';
 import routes from '@/router';
 
 Vue.use(VueRouter);
 
 export default new Vue({
+  store,
   el: '#app',
   components: { App },
   router: new VueRouter({ mode: 'history', routes }),
