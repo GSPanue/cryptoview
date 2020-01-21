9df80db97100bb2ed3e5eb53668ae33a2adca2fb
Gurdev S. Panue <gpanue@hotmail.com>
2020-01-21 21:26:32 +0000

    Import Chart component

--- web/src/index.js
+++ web/src/index.js
@@ -1,19 +1,22 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import { Row, Card, Button } from 'element-ui';
+import { Row, Col, Card, Button } from 'element-ui';
 
 import App from '@/App';
 import store from '@/store';
 import routes from '@/router';
 
 import BaseChart from '@/components/BaseChart/Index';
+import Chart from '@/components/Chart/Index';
 
 Vue.use(VueRouter);
 Vue.use(Row);
+Vue.use(Col);
 Vue.use(Card);
 Vue.use(Button);
 
 Vue.component('base-chart', BaseChart);
+Vue.component('chart', Chart);
 
 export default new Vue({
   store,
