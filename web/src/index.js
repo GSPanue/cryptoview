d62187f44e51a35f3d5094a02533c67e3ad9bf5a
Gurdev S. Panue <gpanue@hotmail.com>
2020-01-21 19:20:46 +0000

    Imported row and chart component

--- web/src/index.js
+++ web/src/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import { Row } from 'element-ui';
 
 import App from '@/App';
 import store from '@/store';
 import routes from '@/router';
 
+import Chart from '@/components/Chart/Index';
+
 Vue.use(VueRouter);
+Vue.use(Row);
+
+Vue.component('chart', Chart);
 
 export default new Vue({
   store,
