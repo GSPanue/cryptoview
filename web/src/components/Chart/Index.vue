37937ac07b372404aefbc47941d037d606116861
Gurdev S. Panue <gpanue@hotmail.com>
2020-01-21 21:26:16 +0000

    Added Chart component

---
+++ web/src/components/Chart/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-col>
+    <el-col class="header">
+      <el-row type="flex">
+        <h3>{{ this.currency }} (${{ this.ticker }})</h3>
+      </el-row>
+      <el-row type="flex">
+        <h4>Price Movements</h4>
+      </el-row>
+    </el-col>
+    <el-col>
+      <base-chart :currency="this.ticker" />
+    </el-col>
+  </el-col>
+</template>
+
+<script>
+export default {
+  props: [
+    'currency',
+    'ticker'
+  ]
+}
+</script>
+
+<style scoped>
+.header {
+  margin-left: 11px;
+}
+</style>
