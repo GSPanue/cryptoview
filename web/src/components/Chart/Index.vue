7772709dc55b111e3695c07ab805e88205a75549
Gurdev S. Panue <gpanue@hotmail.com>
2020-01-21 19:19:56 +0000

    Added Chart component

---
+++ web/src/components/Chart/Index.vue
@@ -0,0 +1,129 @@
+<template>
+  <vue-apex-charts
+    class="chart"
+    type="area"
+    :height="createHeight"
+    :options="createOptions"
+    :series="createSeries"
+  />
+</template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts';
+import { mapGetters } from 'vuex';
+
+export default {
+  props: [
+    'height',
+    'currency'
+  ],
+  components: {
+    VueApexCharts
+  },
+  computed: {
+    ...mapGetters([
+      'getNumericalData'
+    ]),
+    createHeight() {
+      const { height } = this;
+
+      return (height) ? height: 350;
+    },
+    createSeries() {
+      const { currency } = this;
+      const numericalData = this.getNumericalData;
+      const currencyData = numericalData[currency];
+
+      return [{
+        name: 'Price (USD)',
+        data: this.getPrices(currencyData)
+      }];
+    },
+    createOptions() {
+      const { currency } = this;
+      const numericalData = this.getNumericalData;
+      const currencyData = numericalData[currency];
+
+      return {
+        chart: {
+          zoom: {
+            enabled: true
+          },
+          toolbar: {
+            show: true,
+            tools: {
+              download: false,
+              selection: false,
+              zoom: true,
+              zoomin: true,
+              zoomout: true,
+              pan: false,
+              reset: true
+            }
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        tooltip: {
+          x: {
+            format: 'dd MMM yyyy'
+          }
+        },
+        labels: this.getTimestamps(currencyData),
+        xaxis: {
+          type: 'datetime',
+          labels: {
+            datetimeFormatter: {
+              year: 'MMM yyyy',
+              month: 'MMM yyyy'
+            }
+          }
+        },
+        yaxis: {
+          opposite: true,
+          decimalsInFloat: 2,
+          labels: {
+            formatter: (value) => (
+              '$' + (value).toLocaleString(undefined, {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+              })
+            )
+          },
+          title: {
+            text: 'Price (USD)'
+          }
+        },
+        legend: {
+          horizontalAlign: 'left'
+        }
+      }
+    }
+  },
+  methods: {
+    getTimestamps(items) {
+      return items.map((item) => (
+        item.data.timestamp
+      ));
+    },
+    getPrices(items) {
+      return items.map((item) => (
+        item.data.price
+      ));
+    }
+  }
+}
+</script>
+
+<style scoped>
+.chart {
+  max-width: 1000px;
+  width: 100%;
+}
+
+div >>> .apexcharts-toolbar {
+  margin-top: -5px;
+  right: 102px;
+}
+</style>
