f25795216940e7620a36b66bad7db33c2dc7919d
Gurdev S. Panue <gpanue@hotmail.com>
2020-01-22 04:03:34 +0000

    Imported table

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import { Row, Col, Card, Button } from 'element-ui';
+import {
+  Row,
+  Col,
+  Card,
+  Table,
+  TableColumn
+} from 'element-ui';
 
 import App from '@/App';
 import store from '@/store';
@@ -8,15 +14,18 @@ import routes from '@/router';
 
 import BaseChart from '@/components/BaseChart/Index';
 import Chart from '@/components/Chart/Index';
+import CryptoTable from '@/components/Table/Index';
 
 Vue.use(VueRouter);
 Vue.use(Row);
 Vue.use(Col);
 Vue.use(Card);
-Vue.use(Button);
+Vue.use(Table);
+Vue.use(TableColumn);
 
 Vue.component('base-chart', BaseChart);
 Vue.component('chart', Chart);
+Vue.component('crypto-table', CryptoTable);
 
 export default new Vue({
   store,
